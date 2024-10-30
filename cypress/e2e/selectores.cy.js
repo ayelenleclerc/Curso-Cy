14f38ccfa8e7155c37b5f71d72e75c54d0bb1cc8
Galiclerc <galiclerc.cursos@gmail.com>
2024-10-30 10:07:02 -0300

    captura de selectores

--- cypress/e2e/selectores.cy.js
+++ cypress/e2e/selectores.cy.js
@@ -1,32 +1,17 @@
-/// <reference types="cypress" />
+/// <reference types= "cypress" />
 
-
-// copiar selectores manualmente
-
-describe("The-internet  Login", ()=>{
+describe('The-internet-herokuapp-login', ()=>{
     beforeEach('Ingresar a la página', ()=>{
         cy.visit('https://the-internet.herokuapp.com/login')
     })
 
-    it('Copiando selectores',()=>{
-        cy.get("#username").type('tomsmith')
+    it("login ok",()=>{
+        cy.get('#username').type('tomsmith')
         cy.get('#password').type('SuperSecretPassword!')
-        cy.get('#login > button').click()
-        cy.get('.button').click()
-    })
-
-    //copia de selectores con la herramienta Cypress Studio.
-
-    it.only('Login con Cypress Studio', ()=>{
+        cy.get('#login > button > i').click()
         /* ==== Generated with Cypress Studio ==== */
-        cy.get('#username').clear().type('tomsmith');
-        cy.get('#password').clear().type('SuperSecretPassword!');
-        cy.get('.fa').click();
         cy.get('#flash').should('be.visible');
-        cy.wait(2000)
         cy.get('.icon-2x').click();
         /* ==== End Cypress Studio ==== */
     })
-
 })
-
